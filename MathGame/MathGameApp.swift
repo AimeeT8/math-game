7fef7d358f834ae999602ba76016edaf2a2d7921
Aimee Temple <52682021+AimeeT8@users.noreply.github.com>
2025-10-01 08:53:25 -0400

    Initial Commit

---
+++ MathGame/MathGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathGameApp.swift
+//  MathGame
+//
+//  Created by Aimee Temple on 2025-10-01.
+//
+
+import SwiftUI
+
+@main
+struct MathGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
