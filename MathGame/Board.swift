1afd652e6d37d1a8b57ce7add6472160297fb2ae
Aimee Temple <52682021+AimeeT8@users.noreply.github.com>
2025-10-06 10:32:50 -0400

    Basic UI for content view showing the grid. enter function so the user can add or remove a cell by tapping.

--- MathGame/Board.swift
+++ MathGame/Board.swift
@@ -58,4 +58,20 @@ class Board: ObservableObject {
         
         userCells = Array(repeating: Array(repeating: 0, count: size), count: size)
     }
+    
+    // press to add, press again to remove
+    func enter(_ number: Int) {
+        if userCells[selectedRow][selectedCol] == number {
+            userCells[selectedRow][selectedCol] = 0
+        } else {
+            userCells[selectedRow][selectedCol] = number
+            
+            if selectedCol < exampleCells[0].count - 1 {
+                selectedCol += 1
+            } else if selectedRow < exampleCells.count - 1 {
+                selectedRow += 1
+                selectedCol = 0 
+            }
+        }
+    }
 }
--- MathGame/ContentView.swift
+++ MathGame/ContentView.swift
@@ -13,13 +13,33 @@ struct ContentView: View {
     
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                Grid(horizontalSpacing: 2, verticalSpacing: 2) {
+                    ForEach(0..<board.exampleCells.count, id: \.self) { row in
+                        GridRow {
+                            let userRow = board.userCells[row]
+                            
+                            ForEach(0..<userRow.count, id: \.self) { col in
+                                    
+                                let selected = row == board.selectedRow && col == board.selectedCol
+                                
+                                CellView(number: userRow[col], isSelected: selected) {
+                                    board.selectedRow = row
+                                    board.selectedCol = col
+                                }
+                            }
+                        }
+                    }
+                }
+                .padding(.horizontal)
+                
+                Spacer()
+            }
+            .navigationTitle("Math-Game")
         }
-        .padding()
     }
 }
 
